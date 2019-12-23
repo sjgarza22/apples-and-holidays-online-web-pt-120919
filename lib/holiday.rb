45dee2307e9578c8cfd961cdf4aa35bd96b73700
Sara Garza <sjgarza22@gmail.com>
2019-12-23 05:15:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,8 +77,11 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts season.to_s.capitalize + ": "
     holidays.each do |holiday, supplies|
-      puts holiday.to_s.capitalize + ": "
-      supplies.each do
+      print holiday.to_s.capitalize + ": "
+      supplies.each do |supply|
+        print "#{supply}, "
+      end
+      print "\n"
     end
   end
 
