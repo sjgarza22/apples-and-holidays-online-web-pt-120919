a5aa37316044c21482c812d0f9f3be70c712a2ca
Sara Garza <sjgarza22@gmail.com>
2019-12-23 04:32:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 
   holiday_hash.each do |season_hash, holidays|
     if season_hash == season
-      holiday_hash[season_hash] = holiday_name {supply_array}
+      holiday_hash[season_hash][holiday_name] = supply_array
     end
   end
 
