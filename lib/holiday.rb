4979d6a777b54dc98af0b6c56ff54ac25b7a7bc3
Sara Garza <sjgarza22@gmail.com>
2019-12-23 06:18:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -122,4 +122,10 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_array = []
   holiday_hash.each do |season, holidays|
+    holidays.each do |holiday, supplies|
+      if supplies.include?("BBQ")
+        holiday_array << holiday
+      end
+    end
+  end
 end
