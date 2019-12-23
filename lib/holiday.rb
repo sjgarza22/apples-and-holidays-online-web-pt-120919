d87d75210974605a26d2139834c017ef94cf8bfe
Sara Garza <sjgarza22@gmail.com>
2019-12-23 04:39:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,6 +60,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   holiday_hash.each do |season, holidays|
     if season == :winter
+      binding.pry
       holidays.values.flatten
     end
   end
